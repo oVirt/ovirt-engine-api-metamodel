d5212b16111d0b5849f06368dbeac7321e0a8d5e
Martin Perina <mperina@redhat.com>
2021-04-07 21:55:00 +0200

    Align copyright header with other oVirt projects
    Signed-off-by: Martin Perina <mperina@redhat.com>
    Change-Id: I1d4dd7dfdb28fa37daa6d066eab1c1502a78b6f5

--- analyzer/src/main/java/org/ovirt/api/metamodel/analyzer/ModelAnalyzer.java
+++ analyzer/src/main/java/org/ovirt/api/metamodel/analyzer/ModelAnalyzer.java
@@ -1,18 +1,7 @@
 /*
-Copyright (c) 2015-2017 Red Hat, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright oVirt Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 package org.ovirt.api.metamodel.analyzer;
 
@@ -465,7 +454,7 @@ public class ModelAnalyzer {
             //copy from base parameter. Member-involvement-trees not copied on purpose.
             newParameter.setIn(parameter.isIn());
             newParameter.setOut(parameter.isOut());
-            newParameter.setType(parameter.getType());            
+            newParameter.setType(parameter.getType());
             newParameter.setName(parameter.getName());
             newParameter.setDoc(parameter.getDoc());
             newParameter.setSource(parameter.getSource());
